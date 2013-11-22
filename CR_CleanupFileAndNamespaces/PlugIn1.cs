3a3070af0a16d83d569a3bfa8da159e25a63c1b6
Rory Becker <RoryBecker@gmail.com>
2013-11-22 19:40:22 +0000

    Initial Load

---
+++ CR_CleanupFileAndNamespaces/PlugIn1.cs
@@ -0,0 +1,76 @@
+using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using DevExpress.CodeRush.Core;
+using DevExpress.CodeRush.PlugInCore;
+using DevExpress.CodeRush.StructuralParser;
+
+namespace CR_CleanupFileAndNamespaces
+{
+    public partial class PlugIn1 : StandardPlugIn
+    {
+        // DXCore-generated code...
+        #region InitializePlugIn
+        public override void InitializePlugIn()
+        {
+            base.InitializePlugIn();
+            registerCleanupFileAndNamespaces();
+        }
+        #endregion
+        #region FinalizePlugIn
+        public override void FinalizePlugIn()
+        {
+            //
+            // TODO: Add your finalization code here.
+            //
+
+            base.FinalizePlugIn();
+        }
+        #endregion
+        public void registerCleanupFileAndNamespaces()
+        {
+            DevExpress.CodeRush.Core.Action CleanupFileAndNamespaces = new DevExpress.CodeRush.Core.Action(components);
+            ((System.ComponentModel.ISupportInitialize)(CleanupFileAndNamespaces)).BeginInit();
+            CleanupFileAndNamespaces.ActionName = "CleanupFileAndNamespaces";
+            CleanupFileAndNamespaces.ButtonText = "CleanupFile And Namespaces"; // Used if button is placed on a menu.
+            CleanupFileAndNamespaces.RegisterInCR = true;
+            CleanupFileAndNamespaces.Execute += CleanupFileAndNamespaces_Execute;
+            ((System.ComponentModel.ISupportInitialize)(CleanupFileAndNamespaces)).EndInit();
+        }
+        private void CleanupFileAndNamespaces_Execute(ExecuteEventArgs ea)
+        {
+            // Store Location
+            CodeRush.Markers.Drop(true);
+
+            // Find first NamespaceReference
+            ElementEnumerable Enumerable = new ElementEnumerable(CodeRush.Source.ActiveFileNode, LanguageElementType.NamespaceReference, true);
+            NamespaceReference Reference = Enumerable.OfType<NamespaceReference>().FirstOrDefault();
+
+            if (Reference == null)
+            {
+                // No Namespace References to sort.
+                return;
+            }
+            // Move caret       
+            CodeRush.Caret.MoveTo(Reference.Range.Start);
+
+            // Invoke Refactoring.
+            var Refactoring = CodeRush.Refactoring.Get("Optimize Namespace References");
+            CodeRush.SmartTags.UpdateContext();
+            if (Refactoring.IsAvailable)
+            {
+                Refactoring.Execute();
+            }
+
+            // Cleanup the rest of the file.            
+            DevExpress.CodeRush.Core.Action CleanupAction = CodeRush.Actions.Get("CleanupFile");
+            CleanupAction.DoExecute();
+
+            // Restore Location
+            CodeRush.Markers.Collect();
+        }
+    }
+}
---
+++ CR_CleanupFileAndNamespaces/PlugIn1VsixExtension.cs
@@ -0,0 +1,8 @@
+﻿using System.ComponentModel.Composition;
+using DevExpress.CodeRush.Common;
+
+namespace CR_CleanupFileAndNamespaces
+{
+    [Export(typeof(IVsixPluginExtension))]
+    public class CR_CleanupFileAndNamespacesExtension : IVsixPluginExtension { }
+}
